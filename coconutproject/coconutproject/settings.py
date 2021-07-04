40d10a99160971d481de8f98170a11ffbbb0ee78
hees00 <1p2h3s@naver.com>
2021-07-05 00:01:45 +0900

    main페이지 css 수정중

--- coconutproject/coconutproject/settings.py
+++ coconutproject/coconutproject/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'coconut',
         'USER' : 'root',
-        'PASSWORD' : '9106',
+        'PASSWORD' : '0926',
         'HOST' : '127.0.0.1',
         'PORT' : '3306'
     }
--- coconutproject/coconutproject/urls.py
+++ coconutproject/coconutproject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
 import home.views
 
 urlpatterns = [
@@ -23,3 +25,4 @@ urlpatterns = [
     path('home/',include('home.urls')),
     path('account/',include('account.urls')),
 ]
+urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
